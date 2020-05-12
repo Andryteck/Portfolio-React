5ead4de56e7bef736e439123108b439d853ddb7e
Andryteck <772316andrew@gmail.com>
2020-05-12 15:56:57 +0300

    refactor:projectsBlock and Skills

--- src/MyProjects/Project/Project.tsx
+++ src/MyProjects/Project/Project.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import style from './Project.module.css'
+import Button from "../../common/Button/Button";
 
 type propsType = {
     title: string
     description: string
 }
 
-function Project(props:propsType) {
+function Project(props: propsType) {
     return (
         <div className={style.project}>
-       <div className={style.projectsImage}></div>
+            <div className={style.projectsImage}>
+                <Button title={'Look'}/>
+            </div>
             <h3>{props.title}</h3>
             <span className={style.description}>{props.description}</span>
         </div>
